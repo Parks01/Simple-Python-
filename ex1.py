6ed21e59e3e69c9e11ec317684b0d90d21f03778
Andrew Parks <parksdrew01@gmail.com>
2017-09-05 12:30:51 -0700

    new file

---
+++ ex1.py
@@ -0,0 +1,28 @@
+# This is the how many people we have signed up for this application
+
+firstUser = 5 * 10
+secondUsers = 100 * 5
+thirdUsers = 12 * 23
+fourthUsers = 23 + 100
+fifthUsers = 25 + 100
+sixthUser = fourthUsers + firstUser
+
+
+# Our first round of Users
+print "As of today we have", firstUser, "user"
+
+
+#Our second round of Users
+print "Today we have calculated our second round of users which is",  secondUsers
+
+# Our third round of Users
+print "Today we have calculated our third round of users which is",  thirdUsers
+
+# Our third rounf of Users
+print "Today we have calculated our third round of users which is",  fourthUsers
+
+# Our fourth round of users
+print "Today we have calucalted our", fifthUsers, "which is a company record!"
+
+# Our fifth round of users
+print "Today we have a new comapy high at", sixthUser
