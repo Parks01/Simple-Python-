c917522b5ac8c7124c999482bb7471f797875fcf
Andrew Parks <parksdrew01@gmail.com>
2017-09-05 13:23:18 -0700

    add new functions

--- ex1.py
+++ ex1.py
@@ -2,27 +2,57 @@
 
 firstUser = 5 * 10
 secondUsers = 100 * 5
-thirdUsers = 12 * 23
-fourthUsers = 23 + 100
-fifthUsers = 25 + 100
-sixthUser = fourthUsers + firstUser
+# thirdUsers = 12 * 23
+# fourthUsers = 23 + 100
+# fifthUsers = 25 + 100
+# sixthUser = fourthUsers + firstUser
+#
+#
+# # Our first round of Users
+# print "As of today we have", firstUser, "user"
+#
+#
+# #Our second round of Users
+# print "Today we have calculated our second round of users which is",  secondUsers
+#
+# # Our third round of Users
+# print "Today we have calculated our third round of users which is",  thirdUsers
+#
+# # Our third rounf of Users
+# print "Today we have calculated our third round of users which is",  fourthUsers
+#
+# # Our fourth round of users
+# print "Today we have calucalted our", fifthUsers, "which is a company record!"
+#
+# # Our fifth round of users
+# print "Today we have a new comapy high at", sixthUser
+# car is equal t0 100
+# cars = 100
+# # space_in_a_car is equal to 4
+# space_in_a_car = 4
+# # drivers is equal to 90
+# drivers = 30
+# # passengers is equal to 90
+# passengers = 90
+# # cars_not_driven is equal to 100 - 30
+# cars_not_driven = cars - drivers
+# # cars_driven which 70 is now equal to 30 wich is drivers
+# cars_driven = drivers
+# # 90 * 4
+# carpool_capacity = cars_driven * space_in_a_car
+#assign new variable to passengers and divide that the variable cars_driven
+# average_passenger_per_car = passengers / cars_driven
+#
+# print "There are", cars, "cars available"
+# print "There are only", drivers, "drivers available."
+# print "We can transport", carpool_capacity, "people today"
+# print "We have", passengers, "to carpool today"
+# print "We need to put about", average_passenger_per_car, "in each car"
 
-
-# Our first round of Users
-print "As of today we have", firstUser, "user"
-
-
-#Our second round of Users
-print "Today we have calculated our second round of users which is",  secondUsers
-
-# Our third round of Users
-print "Today we have calculated our third round of users which is",  thirdUsers
-
-# Our third rounf of Users
-print "Today we have calculated our third round of users which is",  fourthUsers
-
-# Our fourth round of users
-print "Today we have calucalted our", fifthUsers, "which is a company record!"
-
-# Our fifth round of users
-print "Today we have a new comapy high at", sixthUser
+cars = 100
+space_in_a_car = 4
+drivers = 30
+passegers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers + 100
+print "My name is Andrew", cars_driven
